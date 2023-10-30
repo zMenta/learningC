e16182154634eda9670cb3bb375f85a7a888f409
moog <brentano.lgmail.com>
2023-10-29 21:24:45 -0300

    class03 exercise 2 refactor

--- prog1/class03_exercises/2.c
+++ prog1/class03_exercises/2.c
@@ -2,28 +2,28 @@
 #include <math.h>
 
 int main(void){
-	int input0;
-	int input1;
+	int input;
+	int last_input;
+	int result = 1;
+	int index = 0;
 
 	// Square-number validator
 	// Returns 1 with the second value is the square number of the first input.
-	printf("Please type a sequence of numbers, type 0 to end the sequence.\n");
+	
+	printf("Digit value:\n");
+	scanf("%d", &input);
+	while (input != 0) {
+		last_input = input;
+		printf("Digit value:\n");
+		scanf("%d", &input);
 
-	do{
-		printf("Type the first value:\n");
-		scanf("%d", &input0);
-		if(input0 == 0){break;}
-
-		printf("Type the second value:\n");
-		scanf("%d", &input1);
-
-		if(pow(input0, 2) == input1){
-			printf("%d\n", 1);
-		}else {
-			printf("%d\n", 0);
+		if(pow(last_input,2) != input && index % 2 == 0){
+			result = 0;
+			printf("result defined\n");
 		}
 
-	} while (input1 != 0);
+		index++;
+	}
 	
-	printf("\n");
+	printf("Result: %d\n", result);
 }
